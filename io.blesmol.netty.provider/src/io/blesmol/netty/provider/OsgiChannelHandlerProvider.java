3b201d0ce280954a170974df89a2d4038f23eb53
Jon Passki <jon@passki.us>
2018-02-25 13:33:04 -0800

    more system out logging for dynamic handler

--- io.blesmol.netty.provider/src/io/blesmol/netty/provider/OsgiChannelHandlerProvider.java
+++ io.blesmol.netty.provider/src/io/blesmol/netty/provider/OsgiChannelHandlerProvider.java
@@ -446,6 +446,24 @@ public class OsgiChannelHandlerProvider extends ChannelInboundHandlerAdapter imp
 	}
 
 	@Override
+	public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+		System.out.println("Dynamic handler read");
+		super.channelRead(ctx, msg);
+	}
+	
+	@Override
+	public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+		System.out.println("Dynamic handler read complete");
+		super.channelReadComplete(ctx);
+	}
+
+	@Override
+	public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
+		System.out.println("Dynamic handler user event triggered");
+		super.userEventTriggered(ctx, evt);
+	}
+
+	@Override
 	public void channelActive(ChannelHandlerContext ctx) throws Exception {
 		System.out.println(String.format("Dynamic handler for channel id '%s' is active", priorConfig.channelId()));
 		super.channelActive(ctx);
