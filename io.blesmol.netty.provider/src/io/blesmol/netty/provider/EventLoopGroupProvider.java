bc76e8433aa97990bb67d08124b5c2f8ecb1f293
Jon Passki <jon@passki.us>
2018-03-02 08:09:57 -0800

    event loop pretty printing

--- io.blesmol.netty.provider/src/io/blesmol/netty/provider/EventLoopGroupProvider.java
+++ io.blesmol.netty.provider/src/io/blesmol/netty/provider/EventLoopGroupProvider.java
@@ -1,6 +1,7 @@
 package io.blesmol.netty.provider;
 
 import java.util.Map;
+import java.util.concurrent.Executor;
 
 import org.osgi.framework.Constants;
 import org.osgi.service.component.annotations.Activate;
@@ -8,6 +9,7 @@ import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.ConfigurationPolicy;
 
 import io.blesmol.netty.api.NettyApi;
+import io.netty.channel.EventLoop;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 
@@ -17,12 +19,12 @@ public class EventLoopGroupProvider extends NioEventLoopGroup {
 	private String pid;
 	private String appName;
 	private String groupName;
-	
+
 	@Activate
 	void activate(NettyApi.EventLoopGroup config, Map<String, Object> properties) {
 		this.appName = config.appName();
 		this.groupName = config.groupName();
-		this.pid = (String)properties.get(Constants.SERVICE_PID);
+		this.pid = (String) properties.get(Constants.SERVICE_PID);
 	}
 
 	@Override
@@ -30,6 +32,11 @@ public class EventLoopGroupProvider extends NioEventLoopGroup {
 		return pid + ":" + appName + ":" + groupName;
 	}
 
-	
+	@Override
+	protected EventLoop newChild(Executor executor, Object... args) throws Exception {
+		final EventLoop eventLoop = super.newChild(executor, args);
+		System.out.println(String.format("%s creating new event loop %s", this, eventLoop));
+		return eventLoop;
+	}
 
 }
